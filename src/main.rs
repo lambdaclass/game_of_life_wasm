ce346cad735c05fb8abcf184126883b0afefc9f1
Martin Fraga <martin.fraga@lambdaclass.com>
2022-02-02 12:45:47 -0300

    center grid
    Since we cannot specify the width and height in wasm, at least we can center the board. Split the remaining space into horizontal and vertical padding and use it to render the grid/cells.

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,20 @@ use macroquad::prelude::*;
 #[macroquad::main(window_configuration)]
 async fn main() {
     let cell_size = 20.0;
+    clear_background(RED);
     loop {
-        clear_background(RED);
-        draw_dead_cells(cell_size);
-        draw_grid(cell_size);
+        let padding = get_board_padding(cell_size);
+        draw_dead_cells(cell_size, padding);
+        draw_grid(cell_size, padding);
         next_frame().await
     }
+}
+
+fn get_board_padding(cell_size: f32) -> (f32, f32) {
+    (
+        (screen_width() % cell_size) / 2.0,
+        (screen_height() % cell_size) / 2.0,
+    )
 }
 
 // Windows size config is ignored in wasm/android
@@ -21,28 +29,28 @@ fn window_configuration() -> Conf {
     }
 }
 
-fn draw_dead_cells(cell_size: f32) {
-    let mut x = 0.0;
-    let mut y = 0.0;
-    while x + cell_size <= screen_width() {
-        while y + cell_size <= screen_height() {
+fn draw_dead_cells(cell_size: f32, (padding_x, padding_y): (f32,f32)) {
+    let mut x = padding_x;
+    let mut y = padding_y;
+    while x + cell_size <= screen_width() - padding_x {
+        while y + cell_size <= screen_height() - padding_y {
             draw_rectangle(x, y, cell_size, cell_size, WHITE);
             y += cell_size;
         }
         x += cell_size;
-        y = 0.0;
+        y = padding_y;
     }
 }
 
-fn draw_grid(step: f32) {
-    let mut x = 0.0;
-    let mut y = 0.0;
-    while x + step <= screen_width() {
-        draw_line(x, 0.0, x, screen_height(), 1.0, LIGHTGRAY);
+fn draw_grid(step: f32, (padding_x, padding_y): (f32,f32)) {
+    let mut x = padding_x;
+    let mut y = padding_y;
+    while (x + step) <= (screen_width() - padding_x) {
+        draw_line(x, padding_y, x, screen_height() - padding_y, 1.0, LIGHTGRAY);
         x += step;
     }
-    while y + step <= screen_height() {
-        draw_line(0.0, y, screen_width(), y, 1.0, LIGHTGRAY);
+    while (y + step) <= (screen_height() - padding_y) {
+        draw_line(padding_x, y, screen_width() - padding_x, y, 1.0, LIGHTGRAY);
         y += step;
     }
 }
