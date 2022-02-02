6a703a1a0ee5e833fc3d9d93716490979181ac19
Martin Fraga <martin.fraga@lambdaclass.com>
2022-02-02 13:47:56 -0300

    add Universe and update cells based on neighbours
    We're currently considering a static window size. Upon resizing, the grid should stay the same but the cell size should adjust
    
    For now all the Cells are inititialized Dead so the changes won't be noticeable
    We currently use a vector to store the grid, but a matrix might be better in the future
    
    The typing in the update function should be reviewed and tidied up.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,88 @@
 use macroquad::prelude::*;
 
+#[derive(Debug, Copy, Clone)]
+enum Cell {
+    Dead = 0,
+    Alive = 1,
+}
+
+impl Cell {
+    pub fn update(self, neigh_count: u32) -> Cell {
+        match self {
+            Cell::Dead if neigh_count == 3 => Cell::Alive,
+            Cell::Alive if neigh_count == 2 => Cell::Alive,
+            Cell::Alive if neigh_count == 3 => Cell::Alive,
+            _ => Cell::Dead
+        }
+    }
+}
+
+struct Universe {
+    width: usize,
+    height: usize,
+    cells: Vec<Cell>,
+}
+
+impl Universe {
+    pub fn new(width: usize, height: usize) -> Universe {
+        Universe {
+            width: width,
+            height: height,
+            cells: vec![Cell::Dead; width * height]
+        }
+    }
+
+    // translates x,y position in a grid to the vector position
+    pub fn grid_pos(&self, x: usize, y: usize) -> usize {
+        y * self.width + x
+    }
+
+    pub fn update(&mut self) {
+       let mut grid_copy = self.cells.clone();
+
+        let neigh_positions = [
+            (-1, -1), (-1, 0), (-1, 1),
+            (0, -1), (0, 1),
+            (1, -1), (1, 0), (1, 1)
+        ];
+
+        for x in 0..self.width {
+            for y in 0..self.height {
+                let cell_pos = self.grid_pos(x, y);
+                let mut alive_neighbours = 0;
+                
+                for (i, j) in neigh_positions.iter() {
+                        if  x as i32 + i < 0 || 
+                            x as i32 + i >= self.width as i32 || 
+                            y as i32 + j < 0 ||
+                            y as i32 + j >= self.height as i32 {
+                            continue;
+                        }
+                        
+                        let neigh_pos = self.grid_pos((x as i32 +i) as usize, (y as i32+j) as usize);
+                        match self.cells[neigh_pos] {
+                            Cell::Alive => alive_neighbours += 1,
+                            Cell::Dead => {}
+                        }
+                        
+                }
+
+                grid_copy[cell_pos] = self.cells[cell_pos].update(alive_neighbours);
+                
+            }
+        } 
+        self.cells = grid_copy;
+    }
+
+}
+
 #[macroquad::main(window_configuration)]
 async fn main() {
     let cell_size = 20.0;
     clear_background(RED);
+    let mut universe = Universe::new((screen_width() / cell_size) as usize, (screen_height() / cell_size) as usize);
     loop {
+        universe.update();
         let padding = get_board_padding(cell_size);
         draw_dead_cells(cell_size, padding);
         draw_grid(cell_size, padding);
