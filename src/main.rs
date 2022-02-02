06d5a368c6bc46a5c020693b1acfd338d055481d
Iván Litteri <ilitteri@fi.uba.ar>
2022-02-02 11:39:30 -0300

    Add window configuration
    PS: window size configuration is ignored in wasm/android (for more information visit https://docs.rs/macroquad/latest/macroquad/window/struct.Conf.html).
    It seems that for now we have to make apps independent of the windows size.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
 use macroquad::prelude::*;
 
-#[macroquad::main("BasicShapes")]
+#[macroquad::main(window_configuration)]
 async fn main() {
     loop {
-        clear_background(RED);
-
-        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
-        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
-        draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
-
-        draw_text("IT WORKS!", 20.0, 20.0, 30.0, DARKGRAY);
-
+        clear_background(WHITE);
         next_frame().await
+    }
+}
+
+// Windows size config is ignored in wasm/android
+fn window_configuration() -> Conf {
+    Conf {
+        window_height: 600,
+        window_width: 1200,
+        fullscreen: false,
+        ..Default::default()
     }
 }
