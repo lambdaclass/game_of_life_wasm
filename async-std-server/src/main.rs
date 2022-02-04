697b3ce823a485be2dd41bc6b8f6c5ba96e4427c
Martin Fraga <martin.fraga@lambdaclass.com>
2022-02-04 16:58:07 -0300

    write back messages exactly how they came
    This will cause errors in for example postman because the response has a
    different grammar

--- async-std-server/src/main.rs
+++ async-std-server/src/main.rs
@@ -3,11 +3,13 @@ use async_std::prelude::*;
 // to use block on instead of the attribute macro
 use async_std::task;
 
-use async_std::net::TcpListener;
+use async_std::net::{TcpListener, TcpStream};
 
 fn main() {
     let fut = connections_loop();
-    task::block_on(fut);
+    if let Err(e) = task::block_on(fut) {
+        eprintln!("{}", e);
+    }
 }
 
 async fn connections_loop() -> std::io::Result<()> {
@@ -16,12 +18,27 @@ async fn connections_loop() -> std::io::Result<()> {
     let mut incoming = listener.incoming();
     while let Some(stream) = incoming.next().await {
         let stream = stream?;
-        if let Ok(addr) = stream.peer_addr() {
-            println!("Incoming connection from: {:?}", addr);
-        } else {
-            println!("Incoming connection");
-        }
+        task::spawn(handle_connection(stream));
     }
 
     Ok(())
+}
+
+async fn handle_connection(stream: TcpStream) {
+    let mut stream = stream;
+    
+    // read incoming data (currently blocking)
+    let mut buf = vec![0; 2048];
+    let n = stream.read(&mut buf).await.unwrap();
+
+    // echo back whatever was sent
+    if n > 0 {
+        stream.write_all(&mut buf[..n]).await.unwrap();
+    }
+
+    if let Ok(addr) = stream.peer_addr() {
+        println!("Incoming connection from: {:?}", addr);
+    } else {
+        println!("Incoming connection");
+    }
 }
