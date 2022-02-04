3f2d9ac7e44168d48429df0411b521dbf6908575
Martin Fraga <martin.fraga@lambdaclass.com>
2022-02-04 12:55:01 -0300

    listen to tcp requests

--- async-std-server/src/main.rs
+++ async-std-server/src/main.rs
@@ -1,4 +1,27 @@
+use async_std::prelude::*;
+
+// to use block on instead of the attribute macro
+use async_std::task;
+
+use async_std::net::TcpListener;
 
 fn main() {
-    println!("Hello, world!");
+    let fut = connections_loop();
+    task::block_on(fut);
+}
+
+async fn connections_loop() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8080").await?;
+
+    let mut incoming = listener.incoming();
+    while let Some(stream) = incoming.next().await {
+        let stream = stream?;
+        if let Ok(addr) = stream.peer_addr() {
+            println!("Incoming connection from: {:?}", addr);
+        } else {
+            println!("Incoming connection");
+        }
+    }
+
+    Ok(())
 }
