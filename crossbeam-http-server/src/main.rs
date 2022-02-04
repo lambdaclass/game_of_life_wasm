22fc2108746a6f3e0c26cd49cba5e1facd0a2cdf
Iván Litteri <ilitteri@fi.uba.ar>
2022-02-04 16:10:57 -0300

    Add TCP Listener that accepts the connections

--- crossbeam-http-server/src/main.rs
+++ crossbeam-http-server/src/main.rs
@@ -1,3 +1,12 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+
+    println!("Serving on http://127.0.0.1:8080");
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        println!("Connection established: {}", stream.peer_addr().unwrap());
+    }
 }
