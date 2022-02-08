2ff056eb0dfe37b87d0307f1a7c45ff579b9811e
IAvecilla <iavecilla@fi.uba.ar>
2022-02-08 11:56:19 -0300

    Integration of http module with tokio server

--- http-server/src/tokio-http-server/main.rs
+++ http-server/src/tokio-http-server/main.rs
@@ -3,6 +3,8 @@ use tokio::{
     net::{TcpListener, TcpStream},
 };
 
+use http::{parse_http_request, HttpMethod, HttpRequest};
+
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
@@ -19,31 +21,21 @@ async fn handle_new_connection(mut stream: TcpStream) {
     let (read_part, mut write_part) = stream.split();
     let mut reader = BufReader::new(read_part);
     reader.read(&mut buffer).await.unwrap();
-    let request = String::from_utf8(buffer.to_vec()).unwrap();
+    let request = parse_http_request(&buffer).unwrap();
     let response = create_response(request).await;
     write_part.write_all(response.as_bytes()).await.unwrap();
 }
 
-async fn create_response(request: String) -> String {
-    let response: String;
-    if request.contains("GET") {
-        response = "HTTP/1.1 200 OK\r\nContent-Length: 2\r\n\r\nOK".to_string();
-    } else if request.contains("POST") {
-        let body = get_body_from_request(request.to_string()).await;
-        response = format!(
-            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
-            body.len(),
-            body,
-        );
-    } else {
-        response = "HTTP/1.1 400".to_string();
+async fn create_response(request: HttpRequest) -> String {
+    match request.method {
+        HttpMethod::GET => "HTTP/1.1 200 OK\r\nContent-Length: 2\r\n\r\nOK".to_string(),
+        HttpMethod::POST => {
+            format!(
+                "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+                request.content.len(),
+                request.content,
+            )
+        }
+        _ => "HTTP/1.1 400".to_string(),
     }
-    response
-}
-
-async fn get_body_from_request(request: String) -> String {
-    let request = request.trim_matches(char::from(0));
-    let splitted_request = request.split("\r\n");
-    let vec: Vec<&str> = splitted_request.collect();
-    vec[vec.len() - 1].to_string()
 }
