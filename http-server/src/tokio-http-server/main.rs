157d72207b741ea7abf6755300aa92a282b3fabc
IAvecilla <iavecilla@fi.uba.ar>
2022-02-14 14:10:02 -0300

    Changes in tokio server for host the wasm game of life

--- http-server/src/tokio-http-server/main.rs
+++ http-server/src/tokio-http-server/main.rs
@@ -1,3 +1,5 @@
+use tokio::fs::File;
+
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt, BufReader},
     net::{TcpListener, TcpStream},
@@ -11,30 +13,49 @@ async fn main() {
     loop {
         let (stream, _) = listener.accept().await.unwrap();
         tokio::spawn(async {
-            handle_new_connection(stream).await;
+            handle_new_connection(stream).await.unwrap();
         });
     }
 }
 
-async fn handle_new_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 1024];
+async fn handle_new_connection(mut stream: TcpStream) -> std::io::Result<()> {
+    let mut buffer = [0; 2048];
     let mut reader = BufReader::new(&mut stream);
     reader.read(&mut buffer).await.unwrap();
     let request = parse_http_request(&buffer).unwrap();
-    let response = create_response(request).await;
-    stream.write_all(response.as_bytes()).await.unwrap();
+    let response = create_response(request).await?;
+    stream.write_all(&response).await?;
+
+    Ok(())
 }
 
-async fn create_response(request: HttpRequest) -> String {
+async fn create_response(request: HttpRequest) -> std::io::Result<Vec<u8>> {
     match request.method {
-        HttpMethod::GET => "HTTP/1.1 200 OK\r\nContent-Length: 2\r\n\r\nOK".to_string(),
-        HttpMethod::POST => {
-            format!(
-                "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
-                request.content.len(),
-                request.content,
+        HttpMethod::GET => {
+            let file_requested_path = ".".to_string() + &request.metadata.resource_path;
+            let mut file = File::open(&file_requested_path).await?;
+            let mut content = Vec::new();
+            file.read_to_end(&mut content).await?;
+            let header = if file_requested_path.ends_with(".wasm") {
+                "Content-type:application/wasm\r\n".to_string()
+            } else {
+                "".to_string()
+            };
+            let mut resp = format!(
+                "HTTP/1.1 200 OK\r\n{}Content-Length:{}\r\n\r\n",
+                header,
+                content.len()
             )
+            .into_bytes();
+            resp.append(&mut content);
+            Ok(resp)
         }
-        _ => "HTTP/1.1 400".to_string(),
+        HttpMethod::POST => Ok(format!(
+            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+            request.content.len(),
+            request.content,
+        )
+        .into_bytes()),
+        _ => Ok("HTTP/1.1 400".to_string().into_bytes()),
     }
 }
